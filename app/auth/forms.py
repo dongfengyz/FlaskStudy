935bb3a9ee4f26e741b9ad116ae7df1078215abf
Mathon <837374908@qq.com>
2016-10-09 13:39:31 +0800

    加入用户注册功能

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,13 +1,34 @@
 # coding=utf-8
 import sys
+
 reload(sys)
 sys.setdefaultencoding("utf-8")
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from ..models import User
+
 
 class LoginForm(Form):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
+
+
+class RegistrationForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$',
+                                                                                         0,
+                                                                                         '用户名只包含字母数字下划线和点')])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', '密码必须匹配')])
+    password2 = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已被注册')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已被使用')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,13 @@
 # coding=utf-8
 import sys
+
 reload(sys)
 sys.setdefaultencoding("utf-8")
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
 from flask_login import login_user, logout_user, login_required
-from ..models import User
-from .forms import LoginForm
+from ..models import User, db
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -27,3 +28,14 @@ def logout():
     logout_user()
     flash('您退出了！ ')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        flash("你现在可以登录了")
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
